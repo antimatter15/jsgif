31aa47bdfe86c83289f553accfbbb7cea44fa7de
antimatter15 <antimatter15@antimatter15-desktop.(none)>
2010-07-23 14:55:29 -0400

    inlined byte-array

--- GIFEncoder.js
+++ GIFEncoder.js
@@ -18,6 +18,28 @@
 	
 	GIFEncoder = function()
 	{
+	
+      function ByteArray(){
+        this.bin = [];
+      }
+
+      ByteArray.prototype.getData = function(){
+	      return this.bin.map(function(a){
+	        return String.fromCharCode(a);
+	      }).join('')
+      }
+      ByteArray.prototype.writeByte = function(val){
+        this.bin.push(val >> 0 & 255);
+      }
+      ByteArray.prototype.writeUTFBytes = function(string){
+        for(var l = string.length, i = 0; i < l; i++)
+          this.writeByte(string.charCodeAt(i));
+      }
+      ByteArray.prototype.writeBytes = function(array, offset, length){
+        for(var l = length || array.length, i = offset||0; i < l; i++)
+          this.writeByte(array[i]);
+      }
+	
 	    var exports = {};
 		/*private*/ var width/*int*/ // image size
   		/*private*/ var height/*int*/;
