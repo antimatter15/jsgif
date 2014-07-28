abcc6b1a3c819fd11205ca44b98dca0bb70fcc31
Jonathan Commins <joncom@gmail.com>
2014-07-28 01:19:32 -0700

    Add author to comments.

--- LZWEncoder.js
+++ LZWEncoder.js
@@ -3,6 +3,7 @@
  * Adapted from Jef Poskanzer's Java port by way of J. M. G. Elliott.
  * @author Kevin Weiner (original Java version - kweiner@fmsware.com)
  * @author Thibault Imbert (AS3 version - bytearray.org)
+ * @author Kevin Kwok (JavaScript version - https://github.com/antimatter15/jsgif)
  * @version 0.1 AS3 implementation
  */
 
--- NeuQuant.js
+++ NeuQuant.js
@@ -23,6 +23,7 @@
  * This class handles Neural-Net quantization algorithm
  * @author Kevin Weiner (original Java version - kweiner@fmsware.com)
  * @author Thibault Imbert (AS3 version - bytearray.org)
+ * @author Kevin Kwok (JavaScript version - https://github.com/antimatter15/jsgif)
  * @version 0.1 AS3 implementation
  */
 
